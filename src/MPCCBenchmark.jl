a48f1d9603a54b0675395de46b49b40a9b5c919f
fpacaud <francoispacaud8@gmail.com>
2026-01-20 14:24:30 +0100

    add ComplementOpt in the dependencies

--- src/MPCCBenchmark.jl
+++ src/MPCCBenchmark.jl
@@ -2,8 +2,12 @@ module MPCCBenchmark
 
 using LinearAlgebra
 using JuMP
+import ComplementOpt
 import ExaPowerIO
 
+# Utils
+include("MOI_utils.jl")
+# Benchmarks
 include("nosnoc/NOSNOC.jl")
 include("epec/macepec.jl")
 include("powersystems/powersystems.jl")
